4a335c184f007ca4ed2f49b6a7b3fba5931233b0
Arun Sharma <arunsharma5a6@gmail.com>
2026-02-08 13:10:15 +0530

    Initial commit - Spring Boot app with Docker & Helm

---
+++ src/main/java/com/test/demo/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.test.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from CI/CD demo!";
+    }
+}
